179531210fa0fd133e21a5cdb9dac06cc588467d
Brian Sheaffer <briansheaffer@hotmail.com>
2014-11-04 13:04:36 -0800

    Simplified the calendar
    Less reliance on multiple sed craziness.

--- gt-calendar.sh
+++ gt-calendar.sh
@@ -8,23 +8,42 @@ CurrentText=${Yellow}
 MonthCurrent=${Color_Off}
 MonthNot=${Yellow}
 
+function rtrim() {
+	local var=$@
+	var="${var%"${var##*[![:space:]]}"}"   # remove trailing whitespace characters
+	echo -n "$var"
+}
+
+function ltrim() {
+	local var=$@
+	var="${var#"${var%%[![:space:]]*}"}"   # remove leading whitespace characters
+	echo -n "$var"
+}
+
+function trim() {
+	local var=$@
+	var=$(ltrim "${var}")
+	var=$(rtrim "${var}")
+	echo -n "$var"
+}
+
 WEEK="Su Mo Tu We Th Fr Sa"
-PREVOut=$(cal $(date -j -v -2m "+%m %Y") | grep .. | tail -1 )
-PREV=$(cal $(date -j -v -1m "+%m %Y") | grep .. | tail +3 )
-CURR=$(cal | tail -n6)
-NEXT=$(cal $(date -j -v +1m "+%m %Y") | grep .. | tail +3 )
-NEXTOut=$(cal $(date -j -v +2m "+%m %Y") | grep .. | tail +3 )
+PREVOut=$(trim "$(cal $(date -j -v -2m "+%m %Y") | grep .. | tail -1 )")
+PREV=$(trim "$(cal $(date -j -v -1m "+%m %Y") | grep .. | tail +3 )")
+CURR=$(trim "$(cal | tail -n6)")
+NEXT=$(trim "$(cal $(date -j -v +1m "+%m %Y") | grep .. | tail +3 )")
+NEXTOut=$(trim "$(cal $(date -j -v +2m "+%m %Y") | grep -v "[A-Za-z]" | head -n 1)")
 LINE=$((($(date +%e)/7)+5))
 
 #Display the Current Date
 printf "${Foreground}"
-echo "$(date +"%a, %b %e" | sed -E 's/([^1]1)$/\1st/' | sed -E 's/([^1]2)$/\1nd/' | sed -E 's/([^1]3)$/\1rd/' | sed -E 's/([0-9])$/\1th/' | sed -e "s/\ \ /\ /" -e "s/$/\,\ $(date -j +%Y/)")" | fmt -c 21 21
+date +"%a, %b. %e" | sed -Ee 's/([^1]1)$/\1st/' -Ee 's/([^1]2)$/\1nd/' -Ee 's/([^1]3)$/\1rd/' -Ee 's/([0-9])$/\1th/' -e 's/  / /g' 
 
 #Display the "week day" header
-printf " ${Color_Off}${Background}$WEEK${Color_Off}${MonthNot}\n" 
+printf "${Color_Off}${Background}$WEEK${Color_Off}\n" 
 
 #Display the calendars
-echo ${PREVOut}${PREV}$CURR$NEXT${NEXTOut} | sed -e s'/$/ /g' -e s'/ /  /g' -e s'/\([^0-9][0-9][^0-9]\)/ \1/g' -e s'/  / /g' -e s"/\(.\{21\}\)/\1#/g" | tr -s '#' '\n' | head -n 14| sed -e s'/^/ /g' -e "5,5 s/ 1 /$(printf "${MonthCurrent}") 1 /" -e "9,$ s/ 1 / $(printf "${MonthNot}")1 /" -e "$LINE,$(($LINE+2)) s/ $(date +%e) / $(printf "${Background}")$(date +%e)$(printf "${Color_Off}${Foreground}") /" 
+printf "${Black}${PREVOut}  ${Yellow}${PREV}  ${Color_Off}${CURR}  ${Yellow}${NEXT}  ${Black}${NEXTOut}" | sed -e "$LINE,$(($LINE+2)) s/ $(date +%e) / $(printf "${Background}")$(date +%e)$(printf "${Color_Off}${Foreground}") /"
 
 #Display the "week day" footer
-printf " ${Color_Off}${Background}${WEEK}${Color_Off} "
+printf "${Color_Off}${Background}${WEEK}${Color_Off} "
