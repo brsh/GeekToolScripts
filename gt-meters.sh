b762f68ddc491f5477a68a4a742af0eff932e75e
Brian Sheaffer <briansheaffer@hotmail.com>
2015-04-30 10:52:00 -0700

    Fixed bug with "long" program names
    Finally! Sometimes, the process list would go wonky dropping the load %
    and injecting line 2’s command instead. It wasn’t consisten so I
    couldn’t figure it out. However, thanks to Outlook for Mac driving load
    to 188.7 (on avg), I figured out the bug happened if 1 space between
    cmd and load! Added a quick sed command to add separator at the 17th
    character and tada!
    
    Also, found a typo with adding … to command names in the mem column.
    Replaced the text in the wrong field. Oops.

--- gt-meters.sh
+++ gt-meters.sh
@@ -20,29 +20,29 @@ function out-batt {
 	fi
 
 	if [ ${doDisplay} -eq 1 ]; then
-		${ScriptLoc}/progbar.sh -l ${Label} -n Yellow -b ${BG} -u Default -t 60 -o Red -w 20 ${bat_percent} 100 ${Width}
+		${ScriptLoc}/progbar.sh -l ${Label} -n Yellow -b ${BG} -u Default -t 40 -o Red -w 20 ${bat_percent} 100 ${Width}
 	fi
 	printf "\n"
 }
 
 function out-processes {
 	#Couldn't make "else" work in awk... didn't try very hard... 
-	printf "$(ps -arcxo "command=App %cpu=Load" | head -7)\n$(ps -amcxo "command=App %mem=Mem" | grep -v "com.apple" | head -7 )" | pr -2 -t | sed "s/\ \ *\ /~/g" | tr "\t" "~" | tr -s "~" |
+	printf "$(ps -arcxo "command=App %cpu=Load" | head -7 | sed -e 's/\(.\{16\}\)./\1~/g')\n$(ps -amcxo "command=App %mem=Mem" | grep -v "com.apple" | head -7 | sed -e 's/\(.\{16\}\)./\1~/g')" | pr -2 -t | sed "s/\ \ *\ /~/g" | tr "\t" "~" | tr -s "~" |
 			awk '
 				BEGIN{FS="~"; off="'${Color_Off}'"; header="'${Yellow}${On_Black}'"; warn="'${Black}${On_Yellow}'"; alert="'${White}${On_Red}'"}
-				$1 == "App" { printf header"%-20s %6s    %-20s %6s"off"\n", "App", "Load %", "App", "Mem %" }
+				$1 == "App" { printf header"%-19s %6s    %-19s %6s"off"\n", "App", "Load %", "App", "Mem %" }
 				$1 != "App" { 
 					if ($2 > -1) cColor=off;
 					if ($2 > 40) cColor=warn;
 					if ($2 > 80) cColor=alert;
 					if ($4 > -1) mColor=off;
-					if ($4 > 20) mColor=warn;
-					if ($4 > 40) mColor=alert;
-					if (length($1)>20) $1=substr($1, 0, 16) "...";
-					if (length($3)>20) $1=substr($3, 0, 16) "...";
-					printf cColor"%-20s %6s"off"    "mColor"%-20s %6s"off"\n", $1, $2, $3, $4; 
+					if ($4 > 30) mColor=warn;
+					if ($4 > 60) mColor=alert;
+					if (length($1)>15) $1=substr($1, 0, 16) "...";
+					if (length($3)>15) $3=substr($3, 0, 16) "...";
+					printf cColor"%-19s %6s"off"    "mColor"%-19s %6s"off"\n", $1, $2, $3, $4; 
 				}
-				END{ printf header"%-20s %6s    %-20s %6s"off"\n", "App", "Load %", "App", "Mem %" }
+				END{ printf header"%-19s %6s    %-19s %6s"off"\n", "App", "Load %", "App", "Mem %" }
 				'
 }
 
